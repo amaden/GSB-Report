6f3e00213a4082036653843b4bba9288c992656d
amaden <maden.ayse@gmail.com>
2014-10-02 17:16:56 +0200

    modification app.php and route.php

--- app/app.php
+++ app/app.php
@@ -21,3 +21,13 @@ $app['dao.drug'] = $app->share(function ($app) {
     $drugDAO->setFamilyDAO($app['dao.family']);
     return $drugDAO;
 });
+
+$app['dao.practitionerType'] = $app->share(function ($app) {
+    return new GSB\DAO\PractitionerTypeDAO($app['db']);
+});
+
+$app['dao.practitioner'] = $app->share(function ($app) {
+    $practitionerDAO = new GSB\DAO\PractitionerDAO($app['db']);
+    $practitionerDAO->setPractitionerTypeDAO($app['dao.practitionerType']);
+    return $practitionerDAO;
+});
