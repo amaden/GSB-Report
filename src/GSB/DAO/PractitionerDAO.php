2b7ad926f46fb7897f744460e6689e52cb614d2c
amaden <maden.ayse@gmail.com>
2014-10-09 15:28:05 +0200

    Mission 1

--- src/GSB/DAO/PractitionerDAO.php
+++ src/GSB/DAO/PractitionerDAO.php
@@ -16,12 +16,12 @@ class PractitionerDAO extends DAO
     }
 
     /**
-     * Returns the list of all practitioner, sorted by name.
+     * Returns the list of all practitioners, sorted by name and first name.
      *
-     * @return array The list of all practitioner.
+     * @return array The list of all practitioners.
      */
     public function findAll() {
-        $sql = "select * from practitioner order by practitioner_name";
+        $sql = "select * from practitioner order by practitioner_name, practitioner_first_name";
         $result = $this->getDb()->fetchAll($sql);
         
         // Converts query result to an array of domain objects
@@ -34,15 +34,38 @@ class PractitionerDAO extends DAO
     }
 
     /**
-     * Returns the list of all practitioners for a given type, sorted by name.
+     * Returns the list of all practitioners for a given type, sorted by name and first name.
      *
-     * @param integer $typeId The type id.
+     * @param integer $typeId The practitioner type id.
      *
      * @return array The list of practitioners.
      */
     public function findAllByType($typeId) {
-        $sql = "select * from practitioner where practitioner_type_id=? order by practitioner_name";
+        $sql = "select * from practitioner where practitioner_type_id=? order by practitioner_name, practitioner_first_name";
         $result = $this->getDb()->fetchAll($sql, array($typeId));
+        
+        // Convert query result to an array of domain objects
+        $practitioners = array();
+        foreach ($result as $row) {
+            $practitionerId = $row['practitioner_id'];
+            $practitioners[$practitionerId] = $this->buildDomainObject($row);
+        }
+        return $practitioners;
+    }
+
+    /**
+     * Returns the list of all practitioners matching a name and/or a city, sorted by name and first name.
+     *
+     * @param string $name The name.
+     * @param string $city The city.
+     *
+     * @return array The list of practitioners.
+     */
+    public function findAllByNameAndCity($name, $city) {
+        $sql = "select * from practitioner where practitioner_name like ? and practitioner_city like ? 
+            order by practitioner_name, practitioner_first_name";
+        // If $name and $city are undefined, the SQL query returns all names (%%) and all cities (%%)
+        $result = $this->getDb()->fetchAll($sql, array('%' . $name . '%', '%' . $city . '%'));
         
         // Convert query result to an array of domain objects
         $practitioners = array();
@@ -67,7 +90,7 @@ class PractitionerDAO extends DAO
         if ($row)
             return $this->buildDomainObject($row);
         else
-            throw new \Exception("No practitioner found for id " . $id);
+            throw new \Exception("No Practitioner found for id " . $id);
     }
 
     /**
@@ -92,5 +115,4 @@ class PractitionerDAO extends DAO
         $practitioner->setType($type);
         return $practitioner;
     }
-    
 }
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd7045452d25ac1c16ab027298860859e::getLoader();
+return ComposerAutoloaderInit2ca1962c1c61b5681ad1d801b2e7bf8f::getLoader();
